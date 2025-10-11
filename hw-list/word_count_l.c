d343793476533872f419c80167a9aa9f1f422240
okami-imako <barkan.makar@gmail.com>
2025-10-11 14:43:02 +0000

    solve word_count_l.c

--- hw-list/word_count_l.c
+++ hw-list/word_count_l.c
@@ -26,32 +26,54 @@
 
 #include "word_count.h"
 
-void init_words(word_count_list_t* wclist) { /* TODO */
+void init_words(word_count_list_t* wclist) {
+  list_init(wclist);
 }
 
 size_t len_words(word_count_list_t* wclist) {
-  /* TODO */
-  return 0;
+  return list_size(wclist);
 }
 
 word_count_t* find_word(word_count_list_t* wclist, char* word) {
-  /* TODO */
+  struct list_elem* e;
+  for (e = list_begin(wclist); e != list_end(wclist); e = list_next(e)) {
+    word_count_t* wc = list_entry(e, word_count_t, elem);
+    if (strcmp(wc->word, word) == 0) {
+      return wc;
+    }
+  }
   return NULL;
 }
 
 word_count_t* add_word(word_count_list_t* wclist, char* word) {
-  /* TODO */
-  return NULL;
+  struct list_elem* e;
+  for (e = list_begin(wclist); e != list_end(wclist); e = list_next(e)) {
+    word_count_t* wc = list_entry(e, word_count_t, elem);
+    if (strcmp(wc->word, word) == 0) {
+      wc->count++;
+      return wc;
+    }
+  }
+  word_count_t* wc = (word_count_t*) malloc(sizeof(word_count_t));
+  wc->word = word;
+  wc->count = 1;
+  list_push_back(wclist, &(wc->elem));
+  return wc;
 }
 
 void fprint_words(word_count_list_t* wclist, FILE* outfile) {
-  /* TODO */
-  /* Please follow this format: fprintf(<file>, "%i\t%s\n", <count>, <word>); */
+  struct list_elem* e;
+  for (e = list_begin(wclist); e != list_end(wclist); e = list_next(e)) {
+    word_count_t* wc = list_entry(e, word_count_t, elem);
+    fprintf(outfile, "%i\t%s\n", wc->count, wc->word);
+  }
 }
 
 static bool less_list(const struct list_elem* ewc1, const struct list_elem* ewc2, void* aux) {
-  /* TODO */
-  return false;
+  word_count_t* wc1 = list_entry(ewc1, word_count_t, elem);
+  word_count_t* wc2 = list_entry(ewc2, word_count_t, elem);
+  bool (*comp)(const word_count_t*, const word_count_t*) = aux;
+  return comp(wc1, wc2);
 }
 
 void wordcount_sort(word_count_list_t* wclist,
